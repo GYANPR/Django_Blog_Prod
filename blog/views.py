b7b3d2666e4557465d1d6bd04bf4f096e929f6dd
Prince Jan <janprince002@gmail.com>
2020-10-01 12:07:21 +0100

    Configured initial views of index and detail templatws

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'posts': Post.objects.all(),
+    }
+    return render(request, 'blog/index.html', context)
+
+
+# we could call this view either with post id or slug
+def detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/detail.html', context)
