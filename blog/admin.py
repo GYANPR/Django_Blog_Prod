1accb97865a8b55bae8dcef65e27433cfb51eafa
Prince Jan <janprince002@gmail.com>
2020-10-01 10:16:03 +0100

    Configured a Custom Admin for Post and Category model

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import *
 
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug', 'pub_date']
+    prepopulated_fields = {'slug': ('title',)}  # when a post is created, slug is automatically filled based on title
+    search_fields = ['title', 'content']    # provides a search bar for searching database
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tag']
+
+
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
